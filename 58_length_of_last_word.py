218b844bb3fe38efe1b0a8d289a2ad22d5f87ab1
Vaibhav <vaibhavkumarjak@gmail.com>
2026-06-13 02:11:10 +0530

    Add example to 58 length of last word

--- 58_length_of_last_word.py
+++ 58_length_of_last_word.py
@@ -1,3 +1,13 @@
+# Example 1:
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+
+
+# Example 2:
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
 class Solution(object):
     def lengthOfLastWord(self, s):
         """
