6863af195075014cdad4fb44db45a9c04b4fd465
Vaibhav <vaibhavkumarjak@gmail.com>
2026-06-15 18:53:36 +0530

    Add two pointer pattern dsa solutions. Two sum when array is sorted

---
+++ two-pointer-pattern-dsa/167_two_sum_II-input_array_Is_sorted.py
@@ -0,0 +1,34 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+# Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+# Your solution must use only constant extra space.
+
+# Example 1:
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+# Example 3:
+# Input: numbers = [-1,0], target = -1
+# Output: [1,2]
+# Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(numbers)
+
+        i=0
+        j=length-1
+        while(i<j):
+            s=numbers[i]+numbers[j]
+            if(s==target):
+                return [i+1,j+1]
+            if(s>target):
+                j-=1
+            else:
+                i+=1
